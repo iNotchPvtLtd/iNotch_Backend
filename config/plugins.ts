dfb5ee1bd202b810dbc04785d2d7f0d68d70d893
iNotch Private Limited <smurugesan@inotchglobal.com>
2025-04-25 04:56:41 +0530

    update s3

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,18 +3,22 @@ export default ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET_NAME'),
-        },
+        s3Options: {
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET')
+          },
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET_NAME')
+          }
+        }
       },
       actionOptions: {
         upload: {},
         uploadStream: {},
-        delete: {},
-      },
-    },
-  },
+        delete: {}
+      }
+    }
+  }
 });
